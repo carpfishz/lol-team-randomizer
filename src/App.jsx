112694c72ea7b1b3fa9f73a92f86aa011408e968
Sangmin Lee <lsml3541@gmail.com>
2022-02-03 18:07:25 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
-import { shuffle } from "./utils.js";
+import { shuffle } from "./utils";
 
 const commonPlayers = [
   "키위련",
